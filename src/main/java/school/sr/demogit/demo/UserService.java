c532e53e884e74cd034370e07f3574109d640584
vitalie <vitalie.v.andrusca@gmail.com>
2026-01-22 15:09:19 +0200

    commit in master and push in master

--- src/main/java/school/sr/demogit/demo/UserService.java
+++ src/main/java/school/sr/demogit/demo/UserService.java
@@ -11,5 +11,6 @@ package school.sr.demogit.demo;
 public class UserService {
     public void helloWorld() {
         System.out.println("без логики");
+        System.out.println("commit in master and push in master");
     }
 }
